a170f22fabdac804c370e50f546d1ef1b9f37ac4
Utkarsh Mandilwar <75298162+utmandilwar@users.noreply.github.com>
2023-12-31 02:40:07 +0530

    serviceprovider code

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ class Main():
         r=serviceprovider()
         while True:
             print("\n----------Main Menu----------")
-            print("\nPress-1 create product")
-            print("Press-2 create customer")
+            print("\nPress-1 Create product")
+            print("\nPress-2 Create customer")
+            print("\nPress-3 Delete product")
+            print("\nPress-4 Delete customer")
+            print("\nPress-5 Add to Cart")
+            print("\nPress-6 Remove from Cart")
+            print("\nPress-7 Show Cart")
+            print("\nPress-8 Place Order")
+            print("\nPress-9 Show Order by customer")
+            
             print("Press-3 exit")
             i=int(input())
             if i==1:
@@ -18,13 +26,42 @@ class Main():
                 r.create_product(product_name, price,description,quantity)
             elif i==2:
                 name=input("Enter name: ")
-                password=input("Enter password ")
-                if r.customer_exists(name,password)>0:
+                password=input("Enter password: ")
+                if r.customer_exists((name,password)>0):
                     print("customer already exists")
                 else:
-                    email=input("Enter mai id: ")
+                    email=input("Enter mail id: ")
                     r.create_customer(name, email, password)
             elif i==3:
+                product_id=input("Enter product id: ")
+                r.delete_product(product_id)
+            
+            elif i==4:
+                customer_id=input("Enter customer id: ")
+                r.delete_customer(customer_id)
+                
+            elif i==5:
+                customer_id=input("Enter customer id: ")
+                product_id=input("Enter product id: ")
+                quantity=input("Enter the product quantity: ")
+                r.addTocart(customer_id,product_id,quantity)
+                
+            elif i==6:
+                customer_id=input("Enter customer id: ")
+                product_id=input("Enter product id: ")
+                r.removefromcart(customer_id,product_id)
+                
+            elif i==7:
+                customer_id=input("Enter customer id: ")
+                r.getallfromcart(customer_id)
+                if len(r.getallfromcart(customer_id)) == 0:
+                    print('\nYour Cart is Empty...')
+                    continue
+                print('\nFollowing are the Cart Items :\n*ProductName-(Qty)*')
+                for product_name, quantity in r.getallfromcart(customer_id).items():
+                    print(f'> {self.retrieveProductObject(name).get_name()}-({quantity})')
+                
+            elif i==8:
                 print("Thank you")
                 break
             else:
--- serviceprovider.py
+++ serviceprovider.py
@@ -23,31 +23,127 @@ class serviceprovider(DBConnection):
             pass#self.close()
 
     def create_customer(self, name, email, password):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
         try:
-            self.open() 
             query = f"INSERT INTO customers (name, email, password) VALUES ('{name}', '{email}', '{password}')"
-            self.c.execute(query)
-            self.mydb.commit()
-            customer_id = self.c.lastrowid
+            c = con.cursor()
+            c.execute(query)
+            con.commit()
+            customer_id = c.lastrowid
             print(f"\nCustomer '{name}' added to the database with ID: {customer_id}\n")
             return customer_id
         except Exception as e:
             print(e)
             return None
         finally:
-            self.close() 
+            pass 
 
     def customer_exists(self, name, password):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
         try:
-            self.open()  
             query = f"SELECT COUNT(*) FROM customers WHERE name = '{name}' AND password = '{password}'"
-            self.c.execute(query)
-            count = self.c.fetchone()[0]  
+            c = con.cursor()
+            c.execute(query)
+            con.commit()
+            #self.c.execute(query)
+            count = c.fetchone()[0]  
             return count
-        
         except Exception as e:
             print(e)
             return False
-        
         finally:
-            self.close()
+            pass
+        
+    def delete_product(self,product_id):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
+        try:
+            query = f"DELETE FROM products WHERE product_id = {product_id}"
+            c = con.cursor()
+            c.execute(query)
+            con.commit()
+            if c.rowcount > 0:
+                print(f"Product with ID: {product_id} deleted successfully.")
+            else:
+                print(f"No product found with ID: {product_id}. Nothing was deleted.")
+            return False
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            pass
+        
+    def delete_customer(self, customer_id):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
+        try:
+            query = f"DELETE FROM customers where customer_id = {customer_id}"
+            c= con.cursor()
+            c.execute(query)
+            con.commit()
+            if c.rowcount > 0:
+                print(f"Customer with ID: {customer_id} deleted successfully.")
+            else:
+                print(f"No customer found with ID: {customer_id}. Nothing was deleted.")
+            return False
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            pass
+        
+    def addTocart(self,customer_id,product_id, quantity):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
+        try:
+            query = f"INSERT INTO cart (customer_id, product_id, quantity) VALUES ('{customer_id}', '{product_id}','{quantity}')"
+            c= con.cursor()
+            c.execute(query)
+            con.commit()
+            print(f"Product ID {product_id} added to the cart for Customer ID {customer_id}. Quantity: {quantity}")
+            return False
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            pass
+        
+    def removefromcart(self,customer_id,product_id,):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
+        try:
+            query = f"DELETE FROM cart WHERE customer_id ={customer_id} AND product_id = {product_id}"
+            c= con.cursor()
+            c.execute(query)
+            con.commit()
+            if c.rowcount > 0:
+                print(f"Product ID {product_id} removed from the cart for Customer ID {customer_id}.")
+            else:
+                print(f"No product found in the cart for Customer ID {customer_id} with Product ID {product_id}. Nothing was removed.")
+            return False
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            pass
+        
+    def getallfromcart(self,customer_id):
+        con = mysql.connector.connect(host="localhost", user="root", password="root", database="ecommerce",port="3306",auth_plugin='mysql_native_password')
+        try:
+            query= f"SELECT product_id, quantity from cart where customer_id= {customer_id}"
+            c= con.cursor()
+            c.execute(query)
+            con.commit()
+            
+            return False
+        except Exception as e:
+            print(e)
+            return None
+        finally:
+            pass
+        
+    
+        
+            
+            
+        
+                
+                
+    
+                
